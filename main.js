6d3154b246b0e63af5d5ef9a7c6db1bec408690c
FLEXCONTACT\arnaldo.badin <arnaldo.badin@flexcontact.com.br>
2018-09-28 17:11:30 -0300

    added logger and server

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 const promise = require("./lib/promise.js");
+
+const Logger = require("./lib/logger.js");
+const logger = new Logger("./logs/", "log", ".txt");
+
+const Server = require("./lib/server.js");
+const server = new Server("localhost", 5000);
+
+server.setRoute("post", "/",
+	(request, reponse) => {
+		let body = request.body;
+		logger.in("Requesting / body: ", body);
+
+		return response.sendStatus(200);
+	}
+);
+
+server.run(
+	(host, port) => {
+		return logger.info("Server is running on " + host + ":" + port);
+	}
+);
+
